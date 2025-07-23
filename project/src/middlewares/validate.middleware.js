93b2cb82bc7a5ffaadbc7a234d98b22c99570296
lourina-karkoa <nanasy893@gmail.com>
2025-07-23 18:39:42 +0300

    validate

--- project/src/middlewares/validate.middleware.js
+++ project/src/middlewares/validate.middleware.js
@@ -1,7 +1,17 @@
 const { validationResult } = require("express-validator");
 
 const validate = (req, res, next) => {
-    
+     try {
+        const result = validationResult(req);
+
+        if(!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array().map(e => e.msg) })
+        }
+
+        next()
+    } catch(error) {
+        throw new Error(error.message)
+    }
 }
 
 module.exports = validate
